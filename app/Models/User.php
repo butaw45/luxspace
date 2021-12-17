280bf3203f26cd18c01a4e6b48633e7c3dc6ef16
Muhammad Haris <kumantheas@gmail.com>
2021-12-17 11:33:15 +0700

    D: Relasi carts

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,7 +64,7 @@ class User extends Authenticatable
     public function transactions(){
         return $this->hasMany(Transaction::class, 'users_id','id');
     }
-    public function carts(){
-        return $this->hasMany(Cart::class, 'users_id','id');
-    }
+    // public function carts(){
+    //     return $this->hasMany(Cart::class, 'users_id','id');
+    // }
 }
