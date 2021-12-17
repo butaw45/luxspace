fe8e508de4c5583d32f516e8ab96d55e2e0082f0
Muhammad Haris <kumantheas@gmail.com>
2021-12-17 11:42:31 +0700

    A: Relasi Produk & fillable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'price', 'slug'
+
+    ];
+
+    public function galleries(){
+        return $this->HasMany(ProductGallery::class, 'product_id', 'id');
+    }
 }
