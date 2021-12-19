844aad16bfb8059fde1bcb29e3453ab2f9a7355b
Muhammad Haris <kumantheas@gmail.com>
2021-12-19 10:09:42 +0700

    A: Fungsi Hapus

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -24,12 +24,18 @@ class ProductController extends Controller
             return DataTables::of($query)
             ->addColumn('action', function($item){
                 return '
-                    <a href="'. route('dashboard.product.edit', $item->id) .'">
-                        Edit
-                    </a>
-                ';
-            })
-            ->editColumn('price', function($item){
+                <a class="inline-block border border-gray-700 bg-gray-700 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline"
+                href="' . route('dashboard.product.edit', $item->id) . '">
+                Edit
+                </a>
+                <form class="inline-block" action="' . route('dashboard.product.destroy', $item->id) . '" method="POST">
+                        <button class="border border-red-500 bg-red-500 text-white rounded-md px-2 py-1 m-2 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline" >
+                            Hapus
+                        </button>
+                            ' . method_field('delete') . csrf_field() . '
+                        </form>';
+                })
+            ->editColumn('price', function ($item) {
                 return number_format($item->price);
             })
             ->rawColumns(['action'])
@@ -115,8 +121,11 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return redirect()->route('dashboard.product.index');
+
     }
 }
